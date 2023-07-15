e9f031ba538ac17d9bef81172b24444cf6bedefc
Azazel74 <hamilton.rodriguez@correounivalle.edu.co>
2023-07-15 13:10:59 -0500

    se agrego el LCD al codigo

--- Estacion.py
+++ Estacion.py
@@ -27,3 +27,28 @@ def sampling_time():
 
 log_file = "/home/pi/Desktop/Datos1.csv"
 
+#LCD
+setRGB(128,128,128)
+
+while True:
+    try:
+     
+        #Temperatura y humedad
+        [ temp,hum ] = dht(dht_sensor_port,dht_sensor_type)
+        print("temp =", temp, "C\thumidity =", hum,"%")
+        
+        if isnan(temp) is True or isnan(hum) is True:
+            raise TypeError('nan error')
+
+        t = str(temp)
+        h = str(hum)
+        
+        #Intensidad luminica
+        sensor_value = grovepi.analogRead(light_sensor)
+        s = str(sensor_value)
+        
+        resistance = (float)(1023 - sensor_value) * 68 / sensor_value
+        r = str(resistance)
+
+        print("sensor_value = %d resistance =%.2f" % (sensor_value,  resistance))
+       # time.sleep(.5)
